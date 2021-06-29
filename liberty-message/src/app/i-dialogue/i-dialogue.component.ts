53eb42b5023325f3f106ed6aae67e375b7d717de
MISTERSIERRA <77048872+MISTERSIERRA@users.noreply.github.com>
2021-06-29 21:41:10 +0200

    maj

--- liberty-message/src/app/i-dialogue/i-dialogue.component.ts
+++ liberty-message/src/app/i-dialogue/i-dialogue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServices } from '../services/data-services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-i-dialogue',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IDialogueComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dataServices: DataServices) {
+    // this.variable = this.dataServices.variable;
   }
+
+  ngOnInit() {}
 
 }
--- liberty-message/src/app/services/data-services.ts
+++ liberty-message/src/app/services/data-services.ts
@@ -6,6 +6,32 @@ export class DataServices {
 
 constructor() {}
 
+// observables
+test$ = new BehaviorSubject<any>("1"); // à modifier
+
+// sorties
+$username = "";
+$newusername = "";
+$password = "";
+$newpassword = "";
+$token = "";
+$target = "";
+$roomname = "";
+$message = "";
+
+// entrées
+response = "";
+name = "";
+status = 'nologged';
+token = 'none';
+number: '';
+messagelist = [];   // contenant les messages [{messagedate: '', author: '', messagetext: ''}, {}]
+roomlist = [];      // contenant les discussions [{target: '', roomname: '', datelastmessage: ''}, {}]
+
+
+resetDataAfterRequest() {
+    console.log("reset des variables");
+}
 
 
 
